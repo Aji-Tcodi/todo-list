2ad6507a7f1031c7a2209983deac6ef0a9cb9df2
Aji Tcodi <>
2022-11-16 12:13:48 +0100

    init commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+# Install Angular if you haven't before
+npm install -g @angular/cli
+
+# Create a blank new Angular app with Angular CLI
+ng new todo-list --routing --styles=scss
+cd todo-list
+
+#Use the CLI to generate some components and a service
+ng generate component
+ng generate service
